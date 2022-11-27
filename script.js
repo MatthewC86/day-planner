32d14222950d7e8339471bda05cd98772904749c
Matthew Courtney <cnymhw86@gmail.com>
2022-11-27 08:58:28 -0500

    added new var and display date within ready function

--- script.js
+++ script.js
@@ -2,20 +2,18 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-var currentDay = $('#current-day');
+
+// ready event 
+$(document).ready(function () {
+
+    var currentDay = $('#current-day');
+
+    $('#currentDay').text(dayjs().format('MMM DD, YYYY'));
 
 
 
-$('#currentDay').text(dayjs().format('MMM DD, YYYY'));
-
-console.log(currentDay);
 
 
-
-
-// $(function ()) {
-// }
-    
 
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -23,7 +21,10 @@ console.log(currentDay);
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-    //
+
+    
+     
+    
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
@@ -33,5 +34,5 @@ console.log(currentDay);
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+    
+});
