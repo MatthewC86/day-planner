50185a8596876d229fa5a01863404b7707bf436b
Matthew Courtney <cnymhw86@gmail.com>
2022-11-27 09:48:31 -0500

    added getItem for local storage

--- script.js
+++ script.js
@@ -54,7 +54,17 @@ $('.time-block').each(function () {
       localStorage.setItem(timeStamp, textInput);
      });
      
-    
+     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+     $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+     $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+     $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+     $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+     $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+     $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+     $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+     $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+     
      
     
     // TODO: Add code to apply the past, present, or future class to each time
